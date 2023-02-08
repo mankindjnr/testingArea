561fd6a9094f920c06dd6b5ff0db0dad4a818abe
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-02-08 11:17:02 -0800

    update

--- AirBnBClone/models/base_model.py
+++ AirBnBClone/models/base_model.py
@@ -9,32 +9,16 @@ import uuid
 class BaseModel:
     """
     the  class BaseModel with its public instances and method
+    *args and **kwargs are optional arguments
     """
     def __init__(self, *args, **kwargs):
-        """
-        __init__ - >a class constructor  called when an object is created to
-        initialize the attributes of the class.
-        the following attr are oublic inst attri
-        """
-        if kwargs:
-            """
-            if kwargs is not empty each key of dictionary is an attribute name
-            __class__, each value of this dictionary is the value of the attrr
-            name
-            create and update are strings, convert to datetime
-            otherwise, if not kwargs create id and created at
-            """
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            self.created_at = datetime.strptime(kwargs["created_at"],
-                                                "%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                "%Y-%m-%dT%H:%M:%S.%f")
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        attrs = {key: (datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f") if
+                       key in ["created_at", "updated_at"] else value)
+                 for key, value in kwargs.items() if key != "__class__"}
 
     def __str__(self):
         """
